a97b53e0d32997fd8ff0b945417c47cc2e0a27b7
Dante O. <verlacdante@gmail.com>
2024-02-06 14:21:41 -0300

    NLW Expert | Aula 01

---
+++ src/app.tsx
@@ -0,0 +1,44 @@
+import logo from './assets/Logo.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+export function App() {
+  return (
+    <div className="mx-auto ml-12 max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="nlw expert" />
+
+      {/* INPUT */}
+      <form action="" className="w-full mt-6">
+        <input 
+          type="text"  
+          placeholder='Busque em suas notas...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          />
+      </form>
+      {/* END INPUT */}
+
+      {/* DIVISOR */}
+      <div className='h-px bg-slate-700' />
+      {/* END DIVISOR */}
+
+      {/* GRID */}
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px] text-sm">
+
+        {/* START NewNoteCard */}
+        <NewNoteCard/>
+        {/* END NewNoteCard */}
+
+        {/* START NoteCard */}
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        {/* END NoteCard */}
+
+      </div>
+      {/* END GRID */}
+
+    </div>
+  )
+}
+
+
---
+++ src/components/new-note-card.tsx
@@ -0,0 +1,12 @@
+export function NewNoteCard() {
+    return (
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className='font-medium text-slate-200'>
+            Adicionar nota
+          </span>
+          <p className='leading-6 text-slate-400'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+    )
+}
